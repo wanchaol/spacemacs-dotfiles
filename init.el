74bac7cb237e953b511e29830fa725426a01d232
Wanchao Liang <wanchaol@uber.com>
2016-11-01 14:55:16 -0700

    change go width to 4

--- init.el
+++ init.el
@@ -50,7 +50,7 @@ values."
      syntax-checking
      ;; version-control
      yaml
-     go
+     (go :variables go-tab-width 4)
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
